476df1e8a6c43fbe9435209fc81dcecc4480c2df
Terry Miller <millerterryt@gmail.com>
2022-11-22 08:33:28 -0500

    Started outlining introduction and hashed out objects.

--- spacegame.py
+++ spacegame.py
@@ -1,79 +1,89 @@
 
 #Beginning script of the game
-print("In a galaxy far, far away... \n Your journey begins on Earth.")
+
+
+#Story dialogue begins
+dialogue = "THE WHOLE SCRIPT"
+
+
+introduction = input(f" {dialogue} This is the beginning of the story")
 
 
 
-user_name = input("What is your name?")
 
-Objects
 
-Class Planet
-	Planet 1
-	Planet 2
-	Planet 3
-	Planet 4
-	Planet 5
-	Planet 6
-	Planet 7
-	Planet 8
-	
-Class Resources
-	Food
-	Water
-	Minerals
-	Ores
-	Fuel
-	Currency
-	
-Class NPC/Trader
-	Trader 1
-	Trader 2
-	Trader 3
-	Trader 4
-	Trader 5
-	Trader 6
-	Trader 7
-	Trader 8
 
-Class Items
-	Suit
-	Energy Collection device
-	Warp Speed Upgrade 
-	Advanced Ship
-	
-Class Interaction
-	Trading Interaction
-	Collecting Interaction
-	Travelling Interaction
-	Eating/Drinking Interaction
-	Quit Interaction
-	
-Class Time
-	Time +1 adds to Age of character
-	
-Class Character
-	Health - minimum food/water
-	Age - max 65
-	Inventory - resources/currency obtained
-	
-Class Ship
-	Fuel - amount
-	Items
-	Speed - increases with x item upgrade
-	
-			
-Class Events
-	Health - below minimum
-	Age - above 65
-	Fuel - =0
-	Resources - =0
 
-Proxima Centauri check (all 4 items)
-	Suit
-	Energy Collection device
-	Warp Speed Upgrade 
-	Advanced Ship
+#user_name = input("What is your name?")
+#
+#Objects
+#
+#Class Planet
+#	Planet 1
+#	Planet 2
+#	Planet 3
+#	Planet 4
+#	Planet 5
+#	Planet 6
+#	Planet 7
+#	Planet 8
+#	
+#Class Resources
+#	Food
+#	Water
+#	Minerals
+#	Ores
+#	Fuel
+#	Currency
+#	
+#Class NPC/Trader
+#	Trader 1
+#	Trader 2
+#	Trader 3
+#	Trader 4
+#	Trader 5
+#	Trader 6
+#	Trader 7
+#	Trader 8
+#
+#Class Items
+#	Suit
+#	Energy Collection device
+#	Warp Speed Upgrade 
+#	Advanced Ship
+#	
+#Class Interaction
+#	Trading Interaction
+#	Collecting Interaction
+#	Travelling Interaction
+#	Eating/Drinking Interaction
+#	Quit Interaction
+#	
+#Class Time
+#	Time +1 adds to Age of character
+#	
+#Class Character
+#	Health - minimum food/water
+#	Age - max 65
+#	Inventory - resources/currency obtained
+#	
+#Class Ship
+#	Fuel - amount
+#	Items
+#	Speed - increases with x item upgrade
+#	
+#			
+#Class Events
+#	Health - below minimum
+#	Age - above 65
+#	Fuel - =0
+#	Resources - =0
+#
+#Proxima Centauri check (all 4 items)
+#	Suit
+#	Energy Collection device
+#	Warp Speed Upgrade 
+#	Advanced Ship
 
 
 
