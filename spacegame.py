ca890b66080c7319b96dadcb29d00def24028144
Terry Miller <millerterryt@gmail.com>
2022-11-23 11:00:27 -0500

    Added spacegame code.

--- spacegame.py
+++ spacegame.py
@@ -1,40 +1,104 @@
+# This is the story file for the spacegame
 
-# Python3 program to show that the variables with a value
-# assigned in the class declaration, are class variables and
-# variables inside methods and constructors are instance
-# variables.
+
+#Beginning
+
+import os
+
+
+
+#class Planet:
+#    def __init__(self):
+#        self.name = ["Earth", "Planet 1"]
+#        self.resources = ["food", "minerals"]
+
+
+class Player:
+    def __init__(self, name, location):
+        self.location = location     #Player attributes
+        self.name = name
+        
+
+    def travel():
+        
+    
+    
+
+player = Player(input, "Earth")
+
+
+
+def start():
+    player.name = input('Enter your name \n')
+    story_1 = "Earth is in trouble. The human civilization has used up all of its resources and will only be able to survive for 65 years. Elon Musk has hired you to help him explore the galaxy and bring back energy from Proxima Centauri." 
+    while True:
+        
+        x = name_confirm = input(f'You entered {player.name}. Confirm? \nType "yes" to confirm, "no" to return \n')  
+        if x == 'yes':
+            os.system('clear')
+            break
+        elif x == 'no':
+            os.system('clear')
+            return start()
+        else:
+            os.system('clear')
+            print("I do not understand you.")
+    
+    print(f'Greetings {player.name}')
+    print(story_1)
+    while True:
+        x = choice = input("Will you help save the Earth? \nType 'yes' or 'no' \n")
+        if choice == 'yes':
+            os.system('clear')
+            print("Let's save the Earth")
+            break
+        elif choice == 'no':
+            os.system('clear')
+            print("That's too bad.")
+            exit()
+        else:
+            os.system('clear')
+            print("I do not understand you.")
+
+
+
+if __name__ == "__main__":
+
+    start()
+    print(player.name)
+    print(player.location)
+
+    player.location = "Mars"
+
+    print(player.location)
+
+    player.name = "Bobby"
+
+    print(f'Hello {player.name}')
+
  
-# Class for Dog
- 
- 
-class Planet:
- 
-    # Class Variable
-    planet = 'planet'
- 
-    # The init method or constructor
-    def __init__(self, name, resources):
- 
-        # Instance Variable
-        self.name = [Earth, Venus]
-        self.resources = [Ore, Gold]
- 
- 
-# Objects of Dog class
-Earth = Planet("Pug", "brown")
-Buzo = Dog("Bulldog", "black")
- 
-print('Earth details:')
-print('Earth is a', Earth.planet)
-print('name: ', Earth.name)
-print('resources: ', Earth.resources)
- 
-print('\nBuzo details:')
-print('Buzo is a', Buzo.animal)
-print('Breed: ', Buzo.breed)
-print('Color: ', Buzo.color)
- 
-# Class variables can be accessed using class
-# name also
-print("\nAccessing class variable using class name")
-print(Dog.animal)
+
+    print(player.location)
+
+
+
+
+
+
+            
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+#End
+
