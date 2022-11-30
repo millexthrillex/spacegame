d1c891326348d68c9fb6c8b1e6e9a7c796866b1a
Terry Miller <millerterryt@gmail.com>
2022-11-30 15:40:04 -0500

    Merged Carlos branch changes with main

--- spacegame.py
+++ spacegame.py
@@ -1,4 +1,5 @@
 import os
+import random
 #Beginning script of the game
 
 
@@ -51,56 +52,6 @@ import os
 #   Energy Collection device
 #   Warp Speed Upgrade 
 #   Advanced Ship
-story_list = []
-
-def open_list():
-    global story_list
-    try:
-        with open('story.txt', mode = 'r') as f:
-            b = f.readlines()
-            story_list = [x.replace('\n', '') for x in b]
-    except FileNotFoundError:
-        open(story.txt, w)
-
-
-def view_story():
-    open_list()
-    for i in range(len(story_list)):
-        user_input = input((story_list[i]))
-    
-
-
-
-#user_name = input("What is your name?")
-#
-#Objects
-#
-class Planet:
-    def __init__(self, name):
-        self.name = name
-
-    def planet(self, resources):
-        self.recources = [food, minerals]
-
-class Player:
-    def __init__(self,location):
-        self.location = location
-        self.name = 'name'
-    
-    def travel(self, location):
-        self.location = location
-def clr():
-    clr = os.system('clear')
-    return clr
-
-def travel_menu():
-        print("Where would you like to travel?")
-        for i in range(len(planet_list)):
-            print(f'{i + 1}) {planet_list[i].name}  ')
-            
-        userInput = int(input())
-        player.travel(planet_list[userInput - 1])
-
 def start():
     player.name = input('Enter your name\n')
     
@@ -133,32 +84,195 @@ def start():
         else:
             clr()
             print("I do not understand you.")
+
+story_list = []
+
+def open_list():
+    global story_list
+    try:
+        with open('story.txt', mode = 'r') as f:
+            b = f.readlines()
+            story_list = [x.replace('\n', '') for x in b]
+    except FileNotFoundError:
+        open(story.txt, w)
+
+
+def view_story():
+    open_list()
+    for i in range(len(story_list)):
+        user_input = input((story_list[i]))
+    
+def clr():
+    clr = os.system('clear')
+    return clr
+
+def travel_menu():
+    clr()
+    print(f'Explorer is on planet {player.location.name}')
+    print("Enter the number of the planet you would like to travel?")
+    for i in range(len(planet_list)):
+        print(f'{i + 1}) {planet_list[i].name}  ')
+            
+    userInput = int(input())
+    player.travel(planet_list[userInput - 1])
+    print(f'Explorer is on planet {player.location.name}')
+    
+
+    
+def menu():
+    clr()
+    print(f'What would you like to do in {player.location .name}? ')
+    print('1)Gather resources \n2)Travel \n3)View inventory \n4)Trade')
+    userInput = int(input())
+    if userInput == 1:
+        gather_resources()
+    elif userInput == 2:
+        travel_menu()
+    elif userInput == 3:
+        view_inv()
+    elif userInput == 4:
+        Trade()        
+
+def view_inv():
+    while True:
+        clr()
+        print(f'Inventory:                Explorer\'s age: {int(player.age)}')
+        for i in range(len(inventory)):
+            if inventory[i].quantity != 0:
+                print(f'{inventory[i].name} x{inventory[i].quantity}')
+        e = input('Enter to return to Menu\n')
+        if e == '':
+            break
+        else:
+            continue
+        
+def gather_resources():
+    clr()
+
+    print(f'What would you like to gather from {player.location.name}? ')
+
+    for i in range(len(items_list)-1):
+        print(f'{i + 1}) {items_list[i].name}')
+
+    userInput = int(input())
+
+    if userInput > 4 or userInput < 1:
+        input('Not an option')
+
+    else:
+    
+        items_list[userInput - 1].quantity = random.choice(range(1, 10))
+
+        inventory[userInput - 1].quantity += items_list[userInput - 1].quantity
+    
+    
+        print(f'Explorer has gathered {inventory[userInput - 1].name} x{items_list[userInput - 1].quantity}')
+        player.age = player.age + (1/52)
+    
+        print('One week has passed')
+        input()
+
+        
+        
+def Trade():
+     clr()
+     print(f'What would you like to trade? \n')
+
+     #U_input = inter(input())
+     
+     for i in range(len(npc_inv) - 1):
+         ran_num = random.choice(range(1, 10))
+         npc_inv[1].quantity += ran_num
+         print(f'{i + 1}) {ran_num}x {items_list[i].name} for {random.choice(range(1, 10))}x space tokens\n' )
+         
+     input()
+
+     
+    
+class Planet:
+    def __init__(self, name):
+        self.name = name
+
+    def planet(self, resources):
+        self.resources = resources
+
+        
+
+class Player:
+    def __init__(self,location, inventory = [], age = 20):
+        self.location = location
+        self.name = 'name'
+        self.inventory = []
+        self.age = age
+        
+    def travel(self, location):
+        self.location = location
+
+
+class Item:
+    def __init__(self, name, quantity = 0):
+        self.name = name
+        self.quantity = quantity
+
+def gather_resources():
+    clr()
+
+    print(f'What would you like to gather from {player.location.name}? ')
+
+    for i in range(len(items_list)-1):
+        print(f'{i + 1}) {items_list[i].name}')
+
+    userInput = int(input())
+
+    if userInput > 4 or userInput < 1:
+        input('Not an option')
+
+    else:
+    
+        items_list[userInput - 1].quantity = random.choice(range(1, 10))
+
+        inventory[userInput - 1].quantity += items_list[userInput - 1].quantity
+    
+    
+        print(f'Explorer has gathered {inventory[userInput - 1].name} x{items_list[userInput - 1].quantity}')
+        player.age = player.age + (1/52)
+    
+        print('One week has passed')
+        input()    
+
+
+            
+
+    
 #   Trading Interaction
-#   Collecting Interaction
-#   Travelling Interaction
 #   Eating/Drinking Interaction
 #   Quit Interaction
-#   
-
-#        travel = input("where would you like to travel? \nPlanet1    Planet2\n")
+#   Time
 
 
 
 if __name__ == "__main__":
     planet_list = [Planet('Earth'), Planet('Mars')]
+                
+    player = Player(planet_list[0])
     
-    player = Player( planet_list[0])
-    
-    start()
-    
-    print(f'Explorer is on planet {player.location.name}')
+    items_list = [Item('food'), Item('minerals'), Item('water'), Item('fuel'), Item('space tokens')]
 
-    travel_menu()
+    inventory = [Item('food'), Item('minerals'), Item('water'), Item('fuel'), Item('space tokens')]
 
-    print(f'Explorer is on planet {player.location.name}')
+    npc_inv = [Item('food'), Item('minerals'), Item('water'), Item('fuel'), Item('space tokens')]
+
+    #npc_trade = random.choice(tuple(npc_inv))
+
+    ran_num = random.choice(range(1, 10))
+
+    
+    #start()
+
+
+    while True:
+        menu()
 
     #player.menu(int(input("What would you like to do here? \n1)Collect resources    2)Trade with NPC    3)Leave Planet\n")))
-
+#
     #if player.menu([(input("What would you like to do here? \n1)Collect resources    2)Trade with NPC    3)Leave Planet\n"))]): pass
-        
-    
